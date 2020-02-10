0c0d53a9069c0077dd2380b8c60583fae9b81324
NanaChrs <mathilde.christiaens@isen.yncrea.fr>
2020-02-10 15:52:34 +0100

    correction api controller

---
+++ lab06-web/src/main/java/yncrea/lab06/web/controller/BookController.java
@@ -0,0 +1,46 @@
+package yncrea.lab06.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import yncrea.lab06.core.entity.Book;
+import yncrea.lab06.core.service.BookService;
+import yncrea.lab06.web.dto.BookDTO;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Map;
+
+@Named
+@Path("/books")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class BookController implements RestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @Inject
+    private BookService bookService;
+
+    @GET
+    public Map<Long, String> getAllBooks(){
+        logger.info("get all Books");
+        return bookService.getAllBooks();
+    }
+
+    @GET
+    @Path("/{id}")
+    public BookDTO getBookById(@PathParam("id") long id){
+        Book book = bookService.getBookById(id);
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setAuthors((List)book.getAuthors());
+        bookDTO.setId(book.getId());
+        bookDTO.setImage(book.getImage());
+        bookDTO.setLanguage(String.valueOf(book.getLanguage()));
+        bookDTO.setTags((List)book.getTags());
+        bookDTO.setTitle(book.getTitle());
+        return bookDTO;
+    }
+}
